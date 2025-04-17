cee8a1ea145fc3e2cb605881d0452e4760b62873
lishifeng <lishifenging@outlook.com>
2025-04-18 01:05:26 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver MPU6050
- * @brief     LibDriver MPU6050 is a full function driver of MPU6050 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver MPU6050 is a full-featured driver of MPU6050 and it can run in both MCU and Linux platform.
  * @version   1.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2022/06/30  <td>1.0      <td>Shifeng Li  <td>first upload
